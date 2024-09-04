3eb3fe7c860792cf9bea390b2364b0d66fd1cde5
GhostSilver98723 <vafin5505@mail.ru>
2024-09-04 17:04:59 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+'use strict';
+// 1 nomer
+// let name;
+// let admin;
+// name = "Джон";
+// admin = name;
+// alert(admin);
+
+// // 2 nomer
+// let city = prompt('Введите название города');
+// let yearOfFoundation = prompt('Введите год образования');
+// let population = prompt('Введите количество населения');
+// let ageCity = 2024 - yearOfFoundation;
+// alert(`Городу ${city} исполнилось ${ageCity} лет с момента его образования. Население - ${population} человек.`);
+
+// 3 nomer
+// let r = prompt('Введите радиус круга');
+// let S = Math.PI * (r ** 2);
+// alert(`Площадь круга с радиусом ${r} равна ${S}`);
+
+// 4 nomer
+let firstNumber = +prompt('Ввведите первое число');
+let secondNumber = +prompt('Ввведите второе число');
+alert (`Сумма = ${firstNumber + secondNumber}, Разность = ${firstNumber - secondNumber}, Частное = ${firstNumber / secondNumber}, Произведение = ${firstNumber * secondNumber} `);
