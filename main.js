0c0470a9c51c8fec29f4d8c76c7b9d1671cd1208
GhostSilver98723 <vafin5505@mail.ru>
2024-09-04 17:09:26 +0300

    pravka

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 'use strict';
 // 1 nomer
-// let name;
-// let admin;
-// name = "Джон";
-// admin = name;
-// alert(admin);
+let name;
+let admin;
+name = "Джон";
+admin = name;
+alert(admin);
 
 // // 2 nomer
-// let city = prompt('Введите название города');
-// let yearOfFoundation = prompt('Введите год образования');
-// let population = prompt('Введите количество населения');
-// let ageCity = 2024 - yearOfFoundation;
-// alert(`Городу ${city} исполнилось ${ageCity} лет с момента его образования. Население - ${population} человек.`);
+let city = prompt('Введите название города');
+let yearOfFoundation = prompt('Введите год образования');
+let population = prompt('Введите количество населения');
+let ageCity = 2024 - yearOfFoundation;
+alert(`Городу ${city} исполнилось ${ageCity} лет с момента его образования. Население - ${population} человек.`);
 
 // 3 nomer
-// let r = prompt('Введите радиус круга');
-// let S = Math.PI * (r ** 2);
-// alert(`Площадь круга с радиусом ${r} равна ${S}`);
+let r = prompt('Введите радиус круга');
+let S = Math.PI * (r ** 2);
+alert(`Площадь круга с радиусом ${r} равна ${S}`);
 
 // 4 nomer
 let firstNumber = +prompt('Ввведите первое число');
